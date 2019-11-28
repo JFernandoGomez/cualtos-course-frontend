d83e735611bb474553e27307f65d2b06f196d633
Fernando Gomez <fgomez@intersog.com>
2019-11-28 16:05:11 -0600

    base setup

--- src/index.js
+++ src/index.js
@@ -1,26 +1,54 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { render } from 'react-dom';
 import Title from './Title';
 import './style.scss';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      title: 'Mflix'
-    };
-  }
+const App = () => {
+  let [title, setTitle] = useState('Mflix');
+  let [movies, setMovies] = useState([]);
+  /*
+    Connect the component and consume the API
+  */
+  useEffect(() => {
+    let url = 'http://localhost:3000/movies';
+    let options = {method: 'GET'};
+    let request = new Request(url, options);
 
-  render() {
-    return (
-      <div>
-        <Title name={this.state.title} />
-        <p>
+    fetch( request )
+      .then(response => {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          throw new Error('Something went wrong on api server!');
+        }
+      })
+      .then(
+        response => {
+          // Explain SpreadOperator
           
-        </p>
-      </div>
-    );
-  }
+          //let newMovies = [...movies, ...response];
+          let newMovies = new Array(...movies);
+          response.map( movie => newMovies.push(movie));
+          
+          // update fetched movies into component
+          setMovies(newMovies);
+        }
+      )
+  },[])
+
+  return (
+    <div>
+      <Title name={title} />
+      {/* 
+        ----- create a component that displays the movies 
+        title,
+        plot,
+        rated
+        year
+        imdb info
+      */}
+    </div>
+  );
 }
 
 render(<App />, document.getElementById('root'));
