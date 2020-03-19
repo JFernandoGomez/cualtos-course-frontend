eb97ca440354015aef576527f2b66a9ea6d7b8a2
JFernandoGomez <j.fernando.go@gmail.com>
2020-03-19 17:53:07 -0600

    mocked movies

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import Title from './Title';
 import MovieList from './MovieList';
 import './style.scss';
+import { movie_mocks } from './mocks/movies';
 
 const App = () => {
   let [title, setTitle] = useState('YOUR_NAME');
@@ -11,7 +12,7 @@ const App = () => {
     Connect the component and consume the API
   */
   useEffect(() => {
-    let url = 'http://localhost:3000/movies';
+    let url = 'http://localhost:3001/movies';
     let options = {method: 'GET'};
     let request = new Request(url, options);
 
@@ -20,14 +21,14 @@ const App = () => {
         if (response.status === 200) {
           return response.json();
         } else {
-          throw new Error('Something went wrong on api server!');
           setMovies(movie_mocks);
+          throw new Error('Something went wrong on api server!');
         }
       })
       .then(
         response => {
           // Explain SpreadOperator
-          
+          console.log('response',response);
           //let newMovies = [...movies, ...response];
           let newMovies = new Array(...movies);
           response.map( movie => newMovies.push(movie));
@@ -35,10 +36,15 @@ const App = () => {
           // update fetched movies into component
           setMovies(newMovies);
         }
+      ).catch(
+        _ => {
+          setMovies(movie_mocks);
+          throw new Error('using mocked movies');
+        }
       )
   },[])
 
-  console.log('movies', JSON.stringify(movies));
+  console.log('movies', movies);
 
   return (
     <React.Fragment>
